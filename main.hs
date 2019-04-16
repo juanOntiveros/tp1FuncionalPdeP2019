6433a2407402d0c297a28b97846b4b93ba879f48
juanOntiveros <ontiverosfuertes@yahoo.com.ar>
2019-04-16 19:46:31 -0300

    Primera implementacion de data Auto, de los trucos y de los personajes del punto 1.

---
+++ main.hs
@@ -0,0 +1,27 @@
+-- Punto 1
+data Auto = Auto {
+    nombre :: String,
+    nafta :: Float,
+    velocidad :: Float,
+    nombreDeSuEnamorade :: String
+    -- Falta Modelar Trucos
+} deriving Show
+
+deReversaRocha :: Auto -> Auto
+deReversaRocha unAuto = unAuto {nafta = ((+ 0.2 * 1000).nafta) unAuto}
+
+impresionar :: Auto -> Auto
+impresionar unAuto = unAuto {velocidad = ((*2).velocidad) unAuto}
+
+nitro :: Auto -> Auto
+nitro unAuto = unAuto {velocidad = ((+15).velocidad) unAuto}
+
+fingirAmor :: Auto -> String -> Auto
+fingirAmor unAuto enamorade = unAuto {nombreDeSuEnamorade = enamorade}
+
+-- Personajes
+rochaMcQueen = Auto "RochaMcQueen" 300 0 "Ronco"
+biankerr = Auto "Biankerr" 500 20 "Tinch"
+gushtav = Auto "Gushtav" 200 130 "PetiLaLinda"
+rodra = Auto "Rodra" 0 50 "Taisa"
+
