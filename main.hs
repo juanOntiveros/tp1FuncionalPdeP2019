f52f67d272a404cad9403f1ade9172831fd1dc7a
juanOntiveros <ontiverosfuertes@yahoo.com.ar>
2019-04-18 21:14:34 -0300

    Cambios en main.hs para poder guardar el tipo Truco como una funcion.

--- main.hs
+++ main.hs
@@ -1,16 +1,19 @@
--- Punto 1
+import Text.Show.Functions
+
+--3.1.1Modelado del Auto
+type Truco = Auto -> Auto
+
 data Auto = Auto {
     nombre :: String,
-    nafta :: Float,
+    nivelDeNafta :: Float,
     velocidad :: Float,
-    nombreDeSuEnamorade :: String,
+    suEnamorade :: String,
     truco :: Truco
 } deriving Show
 
-type Truco = Auto
-
+--3.1.2Trucos-------------------------------------------------------------------------------------------------------------------
 deReversaRocha :: Auto -> Auto
-deReversaRocha unAuto = unAuto {nafta = ((+ 0.2 * 1000).nafta) unAuto}
+deReversaRocha unAuto = unAuto {nivelDeNafta = ((+ 0.2 * 1000).nivelDeNafta) unAuto}
 
 impresionar :: Auto -> Auto
 impresionar unAuto = unAuto {velocidad = ((*2).velocidad) unAuto}
@@ -18,35 +21,37 @@ impresionar unAuto = unAuto {velocidad = ((*2).velocidad) unAuto}
 nitro :: Auto -> Auto
 nitro unAuto = unAuto {velocidad = ((+15).velocidad) unAuto}
 
-fingirAmor :: Auto -> String -> Auto
-fingirAmor unAuto enamorade = unAuto {nombreDeSuEnamorade = enamorade}
+fingirAmor :: String -> Auto -> Auto
+fingirAmor enamorade unAuto = unAuto {suEnamorade = enamorade}
 
--- Personajes
-rochaMcQueen = Auto "RochaMcQueen" 300 0 "Ronco" (deReversaRocha rochaMcQueen)
-biankerr = Auto "Biankerr" 500 20 "Tinch" (impresionar biankerr)
-gushtav = Auto "Gushtav" 200 130 "PetiLaLinda" (nitro gushtav)
-rodra = Auto "Rodra" 0 50 "Taisa" (fingirAmor rodra "Petra")
+--3.1.3Modelado de Autos--------------------------------------------------------------------------------------------------------
+rochaMcQueen = Auto "RochaMcQueen" 300 0 "Ronco" (deReversaRocha)
+biankerr = Auto "Biankerr" 500 20 "Tinch" (impresionar)
+gushtav = Auto "Gushtav" 200 130 "PetiLaLinda" (nitro)
+rodra = Auto "Rodra" 0 50 "Taisa" (fingirAmor "Petra")
 
--- Punto 2 
+--3.2Incrementar Velocidad------------------------------------------------------------------------------------------------------
 esVocal :: Char -> Bool
 esVocal letra = letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'
 
-calcularVocales :: Auto -> Int
-calcularVocales unAuto = (length.(filter esVocal).nombreDeSuEnamorade) unAuto
+calcularVocales :: String -> Int
+calcularVocales = length.(filter esVocal)
 
-calcularAumentoVelocidad :: Int -> Float
-calcularAumentoVelocidad numero
- | numero > 0 && numero < 3 = 15
- | numero > 2 && numero < 5 = 20
- | numero >= 5 = 30
- | otherwise = 0
+potenciarSegun :: Int -> Float
+potenciarSegun numeroDeVocales
+ | numeroDeVocales <= 2 = 15
+ | numeroDeVocales <= 4 = 20
+ | otherwise = 30
+
+sumarPotencia :: Auto -> Float -> Float
+sumarPotencia unAuto = (+ (potenciarSegun.calcularVocales.suEnamorade) unAuto)
 
 incrementarVelocidad :: Auto -> Auto
-incrementarVelocidad unAuto = unAuto {velocidad = ((+ (calcularAumentoVelocidad.calcularVocales) unAuto).velocidad) unAuto}
+incrementarVelocidad unAuto = unAuto {velocidad = ((sumarPotencia unAuto).velocidad) unAuto}
 
--- Punto 3
+--3.3 Puede Realizar Truco-------------------------------------------------------------------------------------------------------
 quedaNafta :: Auto -> Bool
-quedaNafta unAuto = ((> 0).nafta) unAuto
+quedaNafta unAuto = ((> 0).nivelDeNafta) unAuto
 
 tieneVelocidadBaja :: Auto -> Bool
 tieneVelocidadBaja unAuto = ((< 100).velocidad) unAuto
@@ -54,23 +59,23 @@ tieneVelocidadBaja unAuto = ((< 100).velocidad) unAuto
 puedeRealizarTruco :: Auto -> Bool
 puedeRealizarTruco unAuto = ((&& quedaNafta unAuto).tieneVelocidadBaja) unAuto
 
--- Punto 4
+--3.4 Nuevos Trucos--------------------------------------------------------------------------------------------------------------
 comboLoco :: Auto -> Auto
 comboLoco unAuto = (nitro.deReversaRocha) unAuto
 
-queTrucazo :: Auto -> String -> Auto
+queTrucazo :: String -> Auto -> Auto
 queTrucazo unAuto = incrementarVelocidad.(fingirAmor unAuto)
 
 explotarNafta :: Auto -> Float
-explotarNafta unAuto = ((*10).nafta) unAuto
+explotarNafta unAuto = ((*10).nivelDeNafta) unAuto
 
 turbo :: Auto -> Auto
-turbo unAuto = unAuto {nafta = 0, velocidad = ((+ explotarNafta unAuto).velocidad) unAuto}
+turbo unAuto = unAuto {nivelDeNafta = 0, velocidad = ((+ explotarNafta unAuto).velocidad) unAuto}
 
+--- Queda revisar y preguntar esto. 
 
-
-activarTruco :: Auto -> Auto
-activarTruco unAuto = truco unAuto
+realizarTruco :: Auto -> Auto
+realizarTruco unAuto = (truco unAuto) unAuto
 
 cambiarTruco :: Auto -> Truco -> Auto
 cambiarTruco unAuto nuevoTruco = unAuto {truco = nuevoTruco}
