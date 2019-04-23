3c0704ef0fc464a156b05ad418053a77e390e3f6
juanOntiveros <ontiverosfuertes@yahoo.com.ar>
2019-04-23 18:36:45 -0300

    Arreglos en esVocal y la modificacion de velocidad de Auto.

--- main.hs
+++ main.hs
@@ -11,28 +11,38 @@ data Auto = Auto {
     truco :: Truco
 } deriving Show
 
+modificarVelocidad :: (Int->Int) -> Auto -> Auto
+modificarVelocidad modificacion unAuto  = unAuto {velocidad = (modificacion.velocidad) unAuto}
+
 --3.1.2Trucos-------------------------------------------------------------------------------------------------------------------
 deReversaRocha :: Auto -> Auto
 deReversaRocha unAuto = unAuto {nivelDeNafta = ((+ 200).nivelDeNafta) unAuto}
 
 impresionar :: Auto -> Auto
-impresionar unAuto = unAuto {velocidad = ((*2).velocidad) unAuto}
+impresionar = modificarVelocidad (*2)
 
 nitro :: Auto -> Auto
-nitro unAuto = unAuto {velocidad = ((+15).velocidad) unAuto}
+nitro = modificarVelocidad (+15)
 
 fingirAmor :: String -> Auto -> Auto
 fingirAmor enamorade unAuto = unAuto {suEnamorade = enamorade}
 
 --3.1.3Modelado de Autos--------------------------------------------------------------------------------------------------------
+rochaMcQueen :: Auto
 rochaMcQueen = Auto "RochaMcQueen" 300 0 "Ronco" (deReversaRocha)
+
+biankerr :: Auto
 biankerr = Auto "Biankerr" 500 20 "Tinch" (impresionar)
+
+gushtav :: Auto
 gushtav = Auto "Gushtav" 200 130 "PetiLaLinda" (nitro)
+
+rodra :: Auto
 rodra = Auto "Rodra" 0 50 "Taisa" (fingirAmor "Petra")
 
 --3.2Incrementar Velocidad------------------------------------------------------------------------------------------------------
 esVocal :: Char -> Bool
-esVocal letra = letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'
+esVocal letra = elem letra "aeiouAEIOU"
 
 contarVocales :: String -> Int
 contarVocales = length.(filter esVocal)
@@ -44,10 +54,10 @@ potenciarSegun enamorade
  |otherwise = 30
 
 sumarPotencia :: Auto -> (Int -> Int)
-sumarPotencia  = (+).potenciarSegun.suEnamorade
+sumarPotencia = (+).potenciarSegun.suEnamorade
 
 incrementarVelocidad :: Auto -> Auto
-incrementarVelocidad unAuto = unAuto {velocidad= ((sumarPotencia unAuto).velocidad) unAuto }
+incrementarVelocidad unAuto = modificarVelocidad (sumarPotencia unAuto) unAuto
 
 --3.3 Puede Realizar Truco-------------------------------------------------------------------------------------------------------
 quedaNafta :: Auto -> Bool
@@ -72,7 +82,7 @@ explotarNafta unAuto = ((*10).nivelDeNafta) unAuto
 turbo :: Auto -> Auto
 turbo unAuto = unAuto {nivelDeNafta = 0, velocidad = ((+ explotarNafta unAuto).velocidad) unAuto}
 
---- Sujeto a revision. 
+--- Sujeto a revision.
 
 realizarTruco :: Auto -> Auto
 realizarTruco unAuto = (truco unAuto) unAuto
